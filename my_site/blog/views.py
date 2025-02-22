7427f5d1a3b922a7cc3f297fac00fb7d6f78576d
Alexander-Bakshaev <shuriknt@gmail.com>
2025-02-22 10:33:15 +0500

    Добавлены два шаблона detail_by_name и detail_by_number

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -4,16 +4,22 @@ from django.template.loader import render_to_string
 
 
 def main_page(request):
-    response = render_to_string('blog/index.html')
-    return HttpResponse(response)
+    return render(request, 'blog/index.html')
 
 
 def posts(request):
     return render(request, 'blog/list_detail.html')
 
+
 def get_post_by_number(request, number_post):
-    return HttpResponse(f"Здесь содержится информация о посте под номером {number_post}")
+    response = {
+        'number_post': number_post,
+    }
+    return render(request, 'blog/detail_by_number.html', context=response)
 
 
 def get_post(request, name_post):
-    return HttpResponse(f"Здесь содержится информация о посте {name_post}")
+    response = {
+        'name_post': name_post,
+    }
+    return render(request, 'blog/detail_by_name.html', context=response)
